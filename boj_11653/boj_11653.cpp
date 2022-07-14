ae0479d797e26c490c47d8ff4f7f99bb725ba599
Se Lee <ese1997@naver.com>
2022-07-14 19:10:22 +0900

    11653 solved

--- boj_11653/boj_11653.cpp
+++ boj_11653/boj_11653.cpp
@@ -12,19 +12,44 @@ int main() {
             break;
         }
 
-        for (int i = 2; i <= sqrt(n); i++) {
-            int isPrime = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    isPrime++;
+        else if (n == 4) {
+            cout << 2 << endl << 2 << endl;
+            n /= 4;
+        }
+
+        else if (n == 3) {
+            cout << 3 << endl;
+            n /= 3;
+        }
+
+        else if (n == 2) {
+            cout << 2 << endl;
+            n /= 2;
+        }
+        
+        else {
+            int nIsNotPrime = 0;
+
+            for (int i = 2; i <= (int)sqrt(n); i++) {
+                int iIsPrime = 0;
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        iIsPrime++;
+                    }
+                }
+                if (iIsPrime == 2) {     // If i is a Prime Number, iIsPrime == 2
+                    if (n % i == 0) {
+                        cout << i << endl;
+                        n /= i;
+                        nIsNotPrime++;
+                        break;
+                    }
                 }
             }
-            if (isPrime == 2) {     // If i is a Prime Number, isPrime == 0
-                if (n % i == 0) {
-                    cout << i << endl;
-                    n /= i;
-                    break;
-                }
+
+            if (nIsNotPrime == 0) {
+                cout << n << endl;
+                n /= n;
             }
         }
     }
